fc899c9fb5a5edea63da0c29216c41852107a2e0
mustafahincal <mustafahncal@gmail.com>
2022-12-06 19:26:13 +0300

    connect mongo db and update routes

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
+import Post from "./models/Post.js";
 
 const app = express();
 
+//? template engine
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+//? connect db
+mongoose.connect("mongodb://localhost/clean-blog-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get("/", (req, res) => {
-  res.render("index");
+//? middlewares
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//? routes
+app.get("/", async (req, res) => {
+  const posts = await Post.find();
+  res.render("index", {
+    posts,
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
@@ -17,6 +33,11 @@ app.get("/post", (req, res) => {
 });
 app.get("/addpost", (req, res) => {
   res.render("add_post");
+});
+
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
 });
 
 const port = 3000;
