8f1ab0717e06b20fa82bf7d792ff266dd0775920
mustafahincal <mustafahncal@gmail.com>
2022-12-08 22:08:26 +0300

    add individual post page

--- app.js
+++ app.js
@@ -28,8 +28,11 @@ app.get("/", async (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about");
 });
-app.get("/post", (req, res) => {
-  res.render("post");
+app.get("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", {
+    post,
+  });
 });
 app.get("/addpost", (req, res) => {
   res.render("add_post");
